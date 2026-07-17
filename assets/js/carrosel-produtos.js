f6e50add57f1158db5377e586ae395ad9794577d
IvanFuziyama <132317547+IvanFuziyama@users.noreply.github.com>
2026-07-17 19:21:04 -0300

    carrosel da sessao produtos concluido

--- assets/js/carrosel-produtos.js
+++ assets/js/carrosel-produtos.js
@@ -1,11 +1,30 @@
 const imgs = document.querySelector(".section-produtos-conjunto-imagens");
+const botao_esquerdo = document.querySelector(".button-left");
+const botao_direito = document.querySelector(".button-right");
 let posicao = 0;
 
+function atualizar_carrosel(){
+    imgs.style.transform = `translateX(-${(posicao*445)}px)`
+}
+function atualizar_botao(){
+    if(posicao == 0){
+        botao_esquerdo.disabled = true;
+    }else if(posicao == 2){
+        botao_direito.disabled = true;
+    }else{
+        botao_direito.disabled = false;
+        botao_esquerdo.disabled = false;
+    }
+}
 function left(){
-    posicao--
-    console.log("clicou para a esquerda")
+    console.log("clicou para a esquerda");
+    posicao--;
+    atualizar_carrosel();
+    atualizar_botao();
 }
 function right(){
+    console.log("clicou para a direita");
     posicao++;
-    console.log("clicou para a direita")
+    atualizar_carrosel();
+    atualizar_botao();
 }
