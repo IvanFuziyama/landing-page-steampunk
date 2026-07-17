1fa1b48b9914e8b0f23f4758ec412c3ee9f3c1e9
IvanFuziyama <132317547+IvanFuziyama@users.noreply.github.com>
2026-07-17 15:44:11 -0300

    desenvolvendo o carrosel do produtos

---
+++ assets/js/carrosel-produtos.js
@@ -0,0 +1,11 @@
+const imgs = document.querySelector(".section-produtos-conjunto-imagens");
+let posicao = 0;
+
+function left(){
+    posicao--
+    console.log("clicou para a esquerda")
+}
+function right(){
+    posicao++;
+    console.log("clicou para a direita")
+}
