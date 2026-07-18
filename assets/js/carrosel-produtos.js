56b4da0aa2abb04b94b9a2d8108454125b0bc4d7
IvanFuziyama <ivanseiji21@gmail.com>
2026-07-18 18:03:09 -0300

    criando a linha e fazendo o estilo e os botoes para transicao

--- assets/js/carrosel-produtos.js
+++ assets/js/carrosel-produtos.js
@@ -2,27 +2,33 @@ const imgs = document.querySelector(".section-produtos-conjunto-imagens");
 const botao_esquerdo = document.querySelector(".button-left");
 const botao_direito = document.querySelector(".button-right");
 let posicao = 0;
+botao_esquerdo.style.color = "#474747"
 
-function atualizar_carrosel(){
-    imgs.style.transform = `translateX(-${(posicao*445)}px)`
+function atualizar_carrosel() {
+    imgs.style.transform = `translateX(-${(posicao * 445)}px)`
 }
-function atualizar_botao(){
-    if(posicao == 0){
+function atualizar_botao() {
+    if (posicao == 0) {
+        botao_esquerdo.style.color = "#474747";
         botao_esquerdo.disabled = true;
-    }else if(posicao == 2){
+
+    } else if (posicao == 2) {
+        botao_direito.style.color = "#474747";
         botao_direito.disabled = true;
-    }else{
+    } else {
+        botao_esquerdo.style.color = "#F5F5F5"
+        botao_direito.style.color= "#F5F5F5"
         botao_direito.disabled = false;
         botao_esquerdo.disabled = false;
     }
 }
-function left(){
+function left() {
     console.log("clicou para a esquerda");
     posicao--;
     atualizar_carrosel();
     atualizar_botao();
 }
-function right(){
+function right() {
     console.log("clicou para a direita");
     posicao++;
     atualizar_carrosel();
