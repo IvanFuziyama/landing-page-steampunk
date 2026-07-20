7210ece6ce2fc8de8a2398b126574b2e351b390b
IvanFuziyama <ivanseiji21@gmail.com>
2026-07-20 11:36:02 -0300

    section produtos concluido, comecando a desenvovler a section servicos

--- assets/js/carrosel-produtos.js
+++ assets/js/carrosel-produtos.js
@@ -3,23 +3,27 @@ const botao_esquerdo = document.querySelector(".button-left");
 const botao_direito = document.querySelector(".button-right");
 let posicao = 0;
 botao_esquerdo.style.color = "#474747"
+botao_esquerdo.style.cursor = "inherit";
 
 function atualizar_carrosel() {
-    imgs.style.transform = `translateX(-${(posicao * 445)}px)`
+    imgs.style.transform = `translateX(-${(posicao * 425)}px)`
 }
 function atualizar_botao() {
     if (posicao == 0) {
         botao_esquerdo.style.color = "#474747";
         botao_esquerdo.disabled = true;
-
+        botao_esquerdo.style.cursor = "inherit";
     } else if (posicao == 2) {
         botao_direito.style.color = "#474747";
         botao_direito.disabled = true;
+        botao_direito.style.cursor = "inherit";
     } else {
         botao_esquerdo.style.color = "#F5F5F5"
         botao_direito.style.color= "#F5F5F5"
         botao_direito.disabled = false;
         botao_esquerdo.disabled = false;
+        botao_direito.style.cursor = "pointer";
+        botao_esquerdo.style.cursor = "pointer";
     }
 }
 function left() {
