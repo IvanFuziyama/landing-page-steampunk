9f9f7253a9bc0131ea8a6408661ab752669763a3
IvanFuziyama <ivanseiji21@gmail.com>
2026-07-21 20:39:58 -0300

    continuando o desenvolvimento do carrosel galeria

--- assets/js/carrosel-galeria.js
+++ assets/js/carrosel-galeria.js
@@ -1,1 +1,38 @@
-const button_esquerdo
+const imgs_galeria = document.querySelector(".section-galeria-conjunto-imagens")
+const botao_esquerdo_galeria = document.querySelector(".button-left-galeria")
+const botao_direito_galeria = document.querySelector(".button-right-galeria")
+
+let posicao_galeria = 0
+botao_esquerdo_galeria.style.color = "#474747"
+botao_esquerdo_galeria.style.cursor = "inherit";
+function atualizar_carrosel_galeria() {
+    imgs_galeria.style.transform = `translateX(-${(posicao_galeria * 425)}px)`
+}
+function atualizar_botao_galeria() {
+    if (posicao_galeria == 0) {
+        botao_esquerdo_galeria.style.color = "#474747";
+        botao_esquerdo_galeria.disabled = true;
+        botao_esquerdo_galeria.style.cursor = "inherit";
+    } else if (posicao_galeria == 2) {
+        botao_direito_galeria.style.color = "#474747";
+        botao_direito_galeria.disabled = true;
+        botao_direito_galeria.style.cursor = "inherit";
+    } else {
+        botao_esquerdo_galeria.style.color = "#F5F5F5"
+        botao_direito_galeria.style.color= "#F5F5F5"
+        botao_direito_galeria.disabled = false;
+        botao_esquerdo_galeria.disabled = false;
+        botao_direito_galeria.style.cursor = "pointer";
+        botao_esquerdo_galeria.style.cursor = "pointer";
+    }
+}
+function left_galeria() {
+    posicao_galeria--;
+    atualizar_carrosel_galeria();
+    atualizar_botao_galeria();
+}
+function right_galeria() {
+    posicao_galeria++;
+    atualizar_carrosel_galeria();
+    atualizar_botao_galeria();
+}
--- assets/js/carrosel-produtos.js
+++ assets/js/carrosel-produtos.js
@@ -1,40 +1,38 @@
-const imgs = document.querySelector(".section-produtos-conjunto-imagens");
-const botao_esquerdo = document.querySelector(".button-left-produtos");
-const botao_direito = document.querySelector(".button-right-produtos");
-let posicao = 0;
-botao_esquerdo.style.color = "#474747"
-botao_esquerdo.style.cursor = "inherit";
+const imgs_produtos = document.querySelector(".section-produtos-conjunto-imagens");
+const botao_esquerdo_produtos = document.querySelector(".button-left-produtos");
+const botao_direito_produtos = document.querySelector(".button-right-produtos");
+let posicao_produtos = 0;
+botao_esquerdo_produtos.style.color = "#474747"
+botao_esquerdo_produtos.style.cursor = "inherit";
 
-function atualizar_carrosel() {
-    imgs.style.transform = `translateX(-${(posicao * 425)}px)`
+function atualizar_carrosel_produtos() {
+    imgs_produtos.style.transform = `translateX(-${(posicao_produtos * 425)}px)`
 }
-function atualizar_botao() {
-    if (posicao == 0) {
-        botao_esquerdo.style.color = "#474747";
-        botao_esquerdo.disabled = true;
-        botao_esquerdo.style.cursor = "inherit";
-    } else if (posicao == 2) {
-        botao_direito.style.color = "#474747";
-        botao_direito.disabled = true;
-        botao_direito.style.cursor = "inherit";
+function atualizar_botao_produtos() {
+    if (posicao_produtos == 0) {
+        botao_esquerdo_produtos.style.color = "#474747";
+        botao_esquerdo_produtos.disabled = true;
+        botao_esquerdo_produtos.style.cursor = "inherit";
+    } else if (posicao_produtos == 2) {
+        botao_direito_produtos.style.color = "#474747";
+        botao_direito_produtos.disabled = true;
+        botao_direito_produtos.style.cursor = "inherit";
     } else {
-        botao_esquerdo.style.color = "#F5F5F5"
-        botao_direito.style.color= "#F5F5F5"
-        botao_direito.disabled = false;
-        botao_esquerdo.disabled = false;
-        botao_direito.style.cursor = "pointer";
-        botao_esquerdo.style.cursor = "pointer";
+        botao_esquerdo_produtos.style.color = "#F5F5F5"
+        botao_direito_produtos.style.color= "#F5F5F5"
+        botao_direito_produtos.disabled = false;
+        botao_esquerdo_produtos.disabled = false;
+        botao_direito_produtos.style.cursor = "pointer";
+        botao_esquerdo_produtos.style.cursor = "pointer";
     }
 }
 function left_produtos() {
-    console.log("clicou para a esquerda");
-    posicao--;
-    atualizar_carrosel();
-    atualizar_botao();
+    posicao_produtos--;
+    atualizar_carrosel_produtos();
+    atualizar_botao_produtos();
 }
 function right_produtos() {
-    console.log("clicou para a direita");
-    posicao++;
-    atualizar_carrosel();
-    atualizar_botao();
+    posicao_produtos++;
+    atualizar_carrosel_produtos();
+    atualizar_botao_produtos();
 }
