fd946fd0e92477114299d7e159b721ce4588c951
IvanFuziyama <132317547+IvanFuziyama@users.noreply.github.com>
2026-07-28 15:45:33 -0300

    media query da navbar concluido

--- assets/js/navbar-mobile.js
+++ assets/js/navbar-mobile.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector(".nav-menu-mobile");
+const fecharBtn = document.querySelector(".nav-fechar-mobile");
 const navList = document.querySelector(".nav-list-mobile");
-
+const overlay = document.querySelector(".overlay-mobile");
 menuBtn.addEventListener("click", () => {
     navList.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+fecharBtn.addEventListener("click", ()=> {
+    navList.classList.remove("active");
+    overlay.classList.remove("active");
 });
